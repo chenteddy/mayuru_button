f3ae19bf0e2adc8caafd42d2f4ee6a7c149fefeb
s711183122 <s711183122@gm.ntpu.edu.tw>
2024-03-28 11:42:20 +0800

    0328

--- push_git.ps1
+++ push_git.ps1
@@ -1,7 +1,7 @@
 cd ./dist
 git init
 git add .
-git commit -m "0305 - test 2"
+git commit -m "0328 - test 1"
 git branch -M main
 git remote add origin https://github.com/chenteddy/mayuru_button_web.git
 git push -u origin main -f
