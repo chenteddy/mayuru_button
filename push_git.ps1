4df27792da2dfc5c3d7290f3b9c8d049b26f8775
s711183122 <s711183122@gm.ntpu.edu.tw>
2024-08-28 20:26:10 +0800

    20240828 voice gain

--- push_git.ps1
+++ push_git.ps1
@@ -1,7 +1,8 @@
 cd ./dist
 git init
 git add .
-git commit -m "0731- test 1"
+git commit -m "0828- voice gain"
 git branch -M main
 git remote add origin https://github.com/chenteddy/mayuru_button_web.git
 git push -u origin main -f
+cd ../
